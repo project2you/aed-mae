a3cc3ff8bacd16798daa3a33570227bf0befd83e
project2you <project2you@gmail.com>
2024-08-11 08:55:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,47 +21,62 @@ import torch
 def main(args):
     print('job dir: {}'.format(os.path.dirname(os.path.realpath(__file__))))
     print("{}".format(args).replace(', ', ',\n'))
+
+    # กำหนดให้ใช้ GPU ถ้าพร้อมใช้งาน
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
     log_writer = SummaryWriter(log_dir=args.output_dir)
 
-    device = args.device
-    if args.run_type =='train':
+    # โหลด dataset train ถ้า run_type เป็น train
+    if args.run_type == 'train':
         dataset_train = AbnormalDatasetGradientsTrain(args)
         print(dataset_train)
+        print(f"Number of training samples: {len(dataset_train)}")
+
+        # ตรวจสอบว่ามี training samples หรือไม่
+        if len(dataset_train) == 0:
+            raise ValueError("No training samples found, please check the dataset path and files.")
+
         sampler_train = torch.utils.data.RandomSampler(dataset_train)
         data_loader_train = torch.utils.data.DataLoader(
             dataset_train, sampler=sampler_train,
             batch_size=args.batch_size,
-            num_workers=args.num_workers,
+            num_workers=0,  # ตั้งค่า num_workers เป็น 0 เพื่อป้องกันปัญหา worker process
             pin_memory=args.pin_mem,
             drop_last=False,
         )
 
+    # โหลด dataset test สำหรับทั้ง train และ inference
     dataset_test = AbnormalDatasetGradientsTest(args)
     print(dataset_test)
     data_loader_test = torch.utils.data.DataLoader(
-        dataset_test, batch_size=args.batch_size, num_workers=args.num_workers,
+        dataset_test, batch_size=args.batch_size, num_workers=0,  # ตั้งค่า num_workers เป็น 0
         pin_memory=args.pin_mem, drop_last=False,
     )
 
-    # define the model
+    # กำหนด model ตาม dataset
     if args.dataset == 'avenue':
         model = mae_cvt_patch16(norm_pix_loss=args.norm_pix_loss, img_size=args.input_size,
-                                                use_only_masked_tokens_ab=args.use_only_masked_tokens_ab,
-                                                abnormal_score_func=args.abnormal_score_func,
-                                                masking_method=args.masking_method,
-                                                grad_weighted_loss=args.grad_weighted_rec_loss).float()
+                                use_only_masked_tokens_ab=args.use_only_masked_tokens_ab,
+                                abnormal_score_func=args.abnormal_score_func,
+                                masking_method=args.masking_method,
+                                grad_weighted_loss=args.grad_weighted_rec_loss).float()
     else:
         model = mae_cvt_patch8(norm_pix_loss=args.norm_pix_loss, img_size=args.input_size,
-                                                use_only_masked_tokens_ab=args.use_only_masked_tokens_ab,
-                                                abnormal_score_func=args.abnormal_score_func,
-                                                masking_method=args.masking_method,
-                                                grad_weighted_loss=args.grad_weighted_rec_loss).float()
+                               use_only_masked_tokens_ab=args.use_only_masked_tokens_ab,
+                               abnormal_score_func=args.abnormal_score_func,
+                               masking_method=args.masking_method,
+                               grad_weighted_loss=args.grad_weighted_rec_loss).float()
+    
+    # ย้ายโมเดลไปยัง GPU
     model.to(device)
+
+    # เลือกทำ train หรือ inference
     if args.run_type == "train":
         do_training(args, data_loader_test, data_loader_train, device, log_writer, model)
     elif args.run_type == "inference":
-        student = torch.load(args.output_dir + "/checkpoint-best-student.pth")['model']
-        teacher = torch.load(args.output_dir + "/checkpoint-best.pth")['model']
+        student = torch.load(args.output_dir + "/checkpoint-best-student.pth", map_location=device)['model']
+        teacher = torch.load(args.output_dir + "/checkpoint-best.pth", map_location=device)['model']
         for key in student:
             if 'student' in key:
                 teacher[key] = student[key]
@@ -70,21 +85,23 @@ def main(args):
             inference(model, data_loader_test, device, args=args)
 
 
-
 def do_training(args, data_loader_test, data_loader_train, device, log_writer, model):
     print("actual lr: %.2e" % args.lr)
-    # following timm: set wd as 0 for bias and norm layers
+
     param_groups = optim_factory.param_groups_weight_decay(model, args.weight_decay)
     optimizer = torch.optim.AdamW(param_groups, lr=args.lr, betas=(0.9, 0.95))
     print(optimizer)
+    
     loss_scaler = NativeScaler()
     misc.load_model(args=args, model=model, optimizer=optimizer, loss_scaler=loss_scaler)
+    
     print(f"Start training for {args.epochs} epochs")
     start_time = time.time()
     best_micro = 0.0
     best_micro_student = 0.0
-    for epoch in range(args.start_epoch, args.epochs):
+    test_stats = None  # กำหนดค่าเริ่มต้นให้กับ test_stats
 
+    for epoch in range(args.start_epoch, args.epochs):
         train_stats = train_one_epoch(
             model, data_loader_train,
             optimizer, device, epoch,
@@ -93,20 +110,24 @@ def do_training(args, data_loader_test, data_loader_train, device, log_writer, m
         )
         log_stats_train = {**{f'train_{k}': v for k, v in train_stats.items()}, 'epoch': epoch}
 
-        test_stats = test_one_epoch(
-            model, data_loader_test, device, epoch, log_writer=log_writer, args=args
-        )
-        log_stats_test = {**{f'test_{k}': v for k, v in test_stats.items()}, 'epoch': epoch}
+        if len(data_loader_test) > 0:
+            test_stats = test_one_epoch(
+                model, data_loader_test, device, epoch, log_writer=log_writer, args=args
+            )
+            log_stats_test = {**{f'test_{k}': v for k, v in test_stats.items()}, 'epoch': epoch}
+        else:
+            print("Warning: No data in data_loader_test.")
+            log_stats_test = {}
 
         if args.output_dir:
             misc.save_model(args=args, model=model, optimizer=optimizer,
                             loss_scaler=loss_scaler, epoch=epoch, latest=True)
-        if test_stats['micro'] > best_micro:
+        if test_stats and test_stats.get('micro', 0) > best_micro:
             best_micro = test_stats['micro']
             misc.save_model(args=args, model=model, optimizer=optimizer,
                             loss_scaler=loss_scaler, epoch=epoch, best=True)
         if args.start_TS_epoch <= epoch:
-            if test_stats['micro'] > best_micro_student:
+            if test_stats and test_stats.get('micro', 0) > best_micro_student:
                 best_micro_student = test_stats['micro']
                 misc.save_model(args=args, model=model, optimizer=optimizer,
                                 loss_scaler=loss_scaler, epoch=epoch, best=True, student=True)
@@ -116,21 +137,27 @@ def do_training(args, data_loader_test, data_loader_train, device, log_writer, m
                 log_writer.flush()
             with open(os.path.join(args.output_dir, "log_train.txt"), mode="a", encoding="utf-8") as f:
                 f.write(json.dumps(log_stats_train) + "\n")
-            with open(os.path.join(args.output_dir, "log_test.txt"), mode="a", encoding="utf-8") as f:
-                f.write(json.dumps(log_stats_test) + "\n")
+            if log_stats_test:
+                with open(os.path.join(args.output_dir, "log_test.txt"), mode="a", encoding="utf-8") as f:
+                    f.write(json.dumps(log_stats_test) + "\n")
+
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     print('Training time {}'.format(total_time_str))
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, default='avenue')
     args = parser.parse_args()
+
     if args.dataset == 'avenue':
         args = get_configs_avenue()
     else:
-        args = get_configs_shanghai()#
+        args = get_configs_shanghai()
+    
     if args.output_dir:
         Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    
     main(args)
